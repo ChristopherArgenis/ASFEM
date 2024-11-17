678453290db33f6d7255fcbd0a7edcd81c0ca020
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2024-11-16 18:48:25 -0800

    Añadir Rutas del Proyecto
    Rutas para la navegación de las diferentes paginas web.

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
+@app.route('/Iniciar Sesion')
+def login():
+    return "<h1> Inicio de Sesion </h1>"
+
+@app.route('/Solicitud')
+def form():
+    return "<h1> Solicitud </h1>"
+
+@app.route('/Comprobante')
+def output():
     return ""
 
 if __name__ == '__main__':
