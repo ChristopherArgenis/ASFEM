6dddd4d9ff0c39a50170d8d0029c29d69081b71f
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2024-11-16 21:56:35 -0800

    Añadiendo DB (NoSQL) a la Prueba
    Relaciones de la DB.-
    - Integrando Método MongoClient desde pymongo para la conexión a la DB
    - Conexión de la App a la DB
    - Iteracion (Escaneo) a la DB
    Añadir datos a la DB
    - Formato JSON que corresponde
    - Añadiendo también el nuevo dato a la aplicación
    - Agregación a la DB.

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+# Conexion a la Base de Datos
+conexion = MongoClient("mongodb+srv://christopherpreciadosilva:Y4cs6WRIUcDb90Eq@solicitudes.7r9rd.mongodb.net/")
+
+# Conexion de la Aplicacion a la Base de Datos
+app.db = conexion.Almacen
+
+# Iterar en los JSON de la Coleccion.
+names = [name for name in app.db.Solicitudes.find({})]
 
 @app.route('/')
 def login():
@@ -18,8 +27,18 @@ def output():
 def test():
     name = ""
     if request.method == "POST":
+        # Apartado de Adquisicion de Datos
         name = request.form.get("name")
-    return render_template("z_pruebas.html")
+
+        # Formato JSON (NoSQL)
+        parametros = {"Nombre": name}
+
+        # Agregar a la Pagina (Cambio Dinamico)
+        names.append(parametros)
+
+        # Agregar a la Base de Datos
+        app.db.Solicitudes.insert_one(parametros)
+    return render_template("z_pruebas.html", nombres=names)
 
 if __name__ == '__main__':
     app.run()
