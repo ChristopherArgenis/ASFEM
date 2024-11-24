8cd83996dd9a3582cc65c5bc3115f468e10923d0
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2024-11-23 19:11:01 -0800

    Se añade el controlador Database
    Como ultimo paso de los anteriores se registra el controlador Database a la aplicación principal.

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 from flask import Flask, render_template, request
-from pymongo import MongoClient
+from controllers.database import Database
 
 app = Flask(__name__)
-# Conexion a la Base de Datos
-# conexion = MongoClient("mongodb+srv://christopherpreciadosilva:Y4cs6WRIUcDb90Eq@solicitudes.7r9rd.mongodb.net/")
-conexion = MongoClient("mongodb+srv://christopherpreciadosilva:Y4cs6WRIUcDb90Eq@solicitudes.7r9rd.mongodb.net/?retryWrites=true&w=majority&appName=Solicitudes")
-
-# Conexion de la Aplicacion a la Base de Datos
-app.db = conexion.Almacen
-
-# Iterar en los JSON de la Coleccion.
-names = [name for name in app.db.Solicitudes.find({})]
+app.register_blueprint(Database)
 
 @app.route('/')
 def login():
@@ -22,24 +14,7 @@ def form():
 
 @app.route('/Comprobante')
 def output():
-    return ""
-
-@app.route('/pruebas', methods=["GET", "POST"])
-def test():
-    name = ""
-    if request.method == "POST":
-        # Apartado de Adquisicion de Datos
-        name = request.form.get("name")
-
-        # Formato JSON (NoSQL)
-        parametros = {"Nombre": name}
-
-        # Agregar a la Pagina (Cambio Dinamico)
-        names.append(parametros)
-
-        # Agregar a la Base de Datos
-        app.db.Solicitudes.insert_one(parametros)
-    return render_template("z_pruebas.html", nombres=names)
+    return "<h1> Comprabantes </h1>"
 
 if __name__ == '__main__':
     app.run()
