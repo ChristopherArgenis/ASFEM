897fb2714d25a1f176b0e7f89fcee44aae5b1705
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2024-11-26 23:23:18 -0800

    Registrando Login
    Registrando una ruta con Blueprint a la app principal.

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from controllers.database import Database
+from controllers.login import Login
 
 app = Flask(__name__)
 app.register_blueprint(Database)
-
-@app.route('/')
-def login():
-    return "<h1> Inicio de Sesion </h1>"
+app.register_blueprint(Login)
 
 @app.route('/Solicitud')
 def form():
