0710f7c96d300535fe976d2ba9d6a82def1b79c9
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2024-11-27 19:53:05 -0800

    Registro de los controladores a la app principal
    El controlador Solicitud se encarga de manejar la ruta de las solicitudes en el que se ingresan de la misma.
    El controlador Comprobante se encarga de dar al usuario un archivo o mensaje de cual fue el resultado de su Solicitud.

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask
 # from controllers.database import Database
 from controllers.login import Login
+from controllers.solicitud import Solicitud
+from controllers.comprobante import Comprobante
 
 app = Flask(__name__)
 # app.register_blueprint(Database)
 app.register_blueprint(Login)
-
-@app.route('/Solicitud')
-def form():
-    return "<h1> Solicitud </h1>"
-
-@app.route('/Comprobante')
-def output():
-    return "<h1> Comprabantes </h1>"
+app.register_blueprint(Solicitud)
+app.register_blueprint(Comprobante)
 
 if __name__ == '__main__':
     app.run()
