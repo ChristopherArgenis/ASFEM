ea49732795da0fdd796757b1a0069a0bdc1d142c
Omar Aparicio <oc.aparicio@uabc.edu.mx>
2024-11-28 08:49:55 -0800

    Prueba

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-# from controllers.database import Database
+from controllers.database import Database
 from controllers.login import Login
 from controllers.solicitud import Solicitud
 from controllers.comprobante import Comprobante
 
 app = Flask(__name__)
-# app.register_blueprint(Database)
+app.register_blueprint(Database)
 app.register_blueprint(Login)
 app.register_blueprint(Solicitud)
 app.register_blueprint(Comprobante)
