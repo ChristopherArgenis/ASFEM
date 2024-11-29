884c63699ff5ad092c45ca61a4214859b837caad
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2024-11-29 01:43:15 -0800

    Modelo final de la Solicitud
    - Se agrega los atributos que tendrá el modelo en cuestión
    - Dado las entradas que se recibiran.

--- models/solicitud.py
+++ models/solicitud.py
@@ -1,4 +1,9 @@
 class Solicitud:
-    def __init__(self, x):
+    def __init__(self, tratamiento, costo, fecha, t_previos, seguro_Medico, ingreso_f):
         self.comprobado = None
-        self.x = x
+        self.tratamiento = tratamiento
+        self.costo = costo
+        self.fecha = fecha
+        self.t_previos = t_previos
+        self.seguro_Medico = seguro_Medico
+        self.ingreso_f = ingreso_f
